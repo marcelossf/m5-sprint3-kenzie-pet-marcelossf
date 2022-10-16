1dadb534b3e3c7fb9ef307912ac880033615600c
Marcelo Soares <soaresmarcelo94@gmail.com>
2022-10-16 20:58:37 -0300

    diagrama er

--- animals/tests.py
+++ animals/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from animals.models import Animal
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        Animal.objects.create(name="lion", age=6, weight=30, sex="Macho", group={"name": "cão", "scientific_name": "canis familiaris"})
+        Animal.objects.create(name="cat", age=6, weight=30, sex="Femea", group={"name": "gato", "scientific_name": "gatis familiaris"})
+
+    
